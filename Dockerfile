cf7d0daa17edb3d0bb3c10decab29e46827f504d
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-04-03 11:28:48 +0200

    Update Dockerfile and docker-compose.yml, and refactor app.py and index.html

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.8-slim
 
 WORKDIR /app
 
-COPY flask-app/ .
+COPY ./flask-app/requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
--- flask-app/app.py
+++ flask-app/app.py
@@ -3,35 +3,44 @@ from rabbitmq_model import RabbitMQModel
 
 app = Flask(__name__)
 
-RABBITMQ_HOST = 'rabbitmq'
+RABBITMQ_HOST = "rabbitmq"
 RABBITMQ_PORT = 5672
-RABBITMQ_USER = 'user'
-RABBITMQ_PASS = 'password'
-QUEUE_NAME = 'messages'
+RABBITMQ_USER = "user"
+RABBITMQ_PASS = "password"
+QUEUE_NAME = "messages"
 
-rabbitmq_model = RabbitMQModel(RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASS, QUEUE_NAME)
+rabbitmq_model = RabbitMQModel(
+    RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USER, RABBITMQ_PASS, QUEUE_NAME
+)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/fetch_message', methods=['GET'])
+
+@app.route("/fetch_message", methods=["GET"])
 def fetch_message():
     message = rabbitmq_model.receive_messages()
     if message:
-        return jsonify({'message': message})
+        return jsonify({"message": message})
     else:
-        return jsonify({'message': 'Aucun message disponible'})
+        return jsonify({"message": "Aucun message disponible"})
 
-@app.route('/send_message', methods=['POST'])
+
+@app.route("/send_message", methods=["POST"])
 def send_message():
     data = request.json
-    message = data.get('message')
+    message = data.get("message")
     if message:
         rabbitmq_model.send_message(message)
-        return jsonify({'status': 'success', 'message': 'Message envoyé avec succès'})
+        return jsonify({"status": "success", "message": "Message envoyé avec succès"})
     else:
-        return jsonify({'status': 'error', 'message': 'Le champ "message" est requis'}), 400
+        return (
+            jsonify({"status": "error", "message": 'Le champ "message" est requis'}),
+            400,
+        )
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
